47002bd80b0000a530293233da442bf56963f88b
BELLARIVA Bastien <bastien.bellariva@ynov.com>
2018-05-08 17:46:05 +0200

    Avancement multiples

--- Scripts SQL/import_recolte.sql
+++ Scripts SQL/import_recolte.sql
@@ -5,7 +5,6 @@ CREATE TABLE t_recolte_import (
     id_recolte INT NOT NULL AUTO_INCREMENT,
     -- Informations sur les départements
     i_numero_departement VARCHAR(4) NOT NULL,
-    i_libelle_departement VARCHAR(30) NOT NULL,
     -- Informations sur les déclarations de récoltes
     A ECRIRE;
     -- Informations sur la superficie des vignes
@@ -15,6 +14,24 @@ CREATE TABLE t_recolte_import (
     i_surface_igp FLOAT(4) NOT NULL,
     i_surface_vsig FLOAT(4) NOT NULL,
     -- Informations sur les quantités de vin produites
-    i_total_quantite FLOAT(4) NOT NULL,
-    
-)
+    i_quantite_aop_blanc FLOAT(4) NOT NULL,
+    i_quantite_aop_rouge FLOAT(4) NOT NULL,
+    i_quantite_aop_rose FLOAT(4) NOT NULL,
+    i_quantite_aop_vsi FLOAT(4) NOT NULL,
+    i_quantite_igp_blanc FLOAT(4) NOT NULL,
+    i_quantite_igp_rouge FLOAT(4) NOT NULL,
+    i_quantite_igp_rose FLOAT(4) NOT NULL,
+    i_quantite_vsig_blanc FLOAT(4) NOT NULL,
+    i_quantite_vsig_rouge FLOAT(4) NOT NULL,
+    i_quantite_vsig_rose FLOAT(4) NOT NULL,
+    i_quantite_cognac_armagnac FLOAT(4) NOT NULL,
+    i_quantite_commercialisable FLOAT(4) NOT NULL,
+    i_quantite_non_commercialisable FLOAT(4) NOT NULL,
+    i_total_quantite FLOAT(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOAD DATA LOCAL INFILE '../ressources/csv_import/import_recolte_csv.csv' INTO
+TABLE t_recolte_import
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n';
