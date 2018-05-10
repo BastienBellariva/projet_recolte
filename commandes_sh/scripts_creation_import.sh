150093286699c5853c03df0af4115642cd60d58b
BELLARIVA Bastien <bastien.bellariva@ynov.com>
2018-05-10 10:39:47 +0200

    maj script shell improt

--- commandes_sh/scripts_creation_import.sh
+++ commandes_sh/scripts_creation_import.sh
@@ -9,16 +9,21 @@
 
 #Lancement du script "creation-base" : crée la base d_vigne
 mysql -u root -p <../scripts_SQL/creation-base.sql
+echo "La création de la base de données est terminée."
 
 #Lancement du script "import_recolte" : importe toutes les valeurs avec le numéro de département faisant l'unicité des lignes
 #mysql -u root -p d_vigne < ../scripts_SQL/import_recolte.sql
+#echo "L'import de l'ensemble des valeurs s'est déroulé correctement."
 
 #Lancement du script "import_couleur" : importe toutes les couleurs de vin
 #mysql -u root -p d_vigne < ../scripts_SQL/import_couleur.sql
+#echo "La table couleur a été créée et ses valeurs importées correctement."
 
 #Lancement du script "import_departement" : importe tous les départements
 #mysql -u root -p d_vigne < ../scripts_SQL/import_departement.sql
+#echo "La table departement a été créée et ses valeurs importées correctement."
 
 #Lancement du script "import_type" : importe tous les types
 #mysql -u root -p d_vigne < ../scripts_SQL/import_type.sql
+#echo "La table type a été créée et ses valeurs importées correctement."
 
