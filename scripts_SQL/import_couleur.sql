ebd300eff84559bda489f892223e5b166f3bbf84
BastienBellariva <bastien.bellariva@ynov.com>
2018-05-10 11:27:10 +0200

    passage sur le WD

--- scripts_SQL/import_couleur.sql
+++ scripts_SQL/import_couleur.sql
@@ -1,14 +1,14 @@
 -- Table couleur : stocke toutes les couleurs de vin
 CREATE TABLE t_couleur (
-  id_couleur INT(4) NOT NULL AUTO_INCREMENT,
+  -- id_couleur INT(4) NOT NULL AUTO_INCREMENT,
   libelle_couleur VARCHAR(30) NOT NULL,
-  information_couleur VARCHAR(80) NOT NULL,
-  CONSTRAINT pk_id_couleur PRIMARY KEY(id_couleur)
+  information_couleur VARCHAR(80) NOT NULL
+  -- CONSTRAINT pk_id_couleur PRIMARY KEY(id_couleur)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOAD DATA LOCAL INFILE '../ressources/csv_import/import_couleur_csv.csv' INTO
-TABLE t_couleur
+LOAD DATA LOCAL INFILE '../ressources/csv_import/import_couleur_csv.csv'
+INTO TABLE t_couleur
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n';
+LINES TERMINATED BY '\n';
 
