be506082ad05b9f20f6cae7c26838a126f7ec9a2
BastienBellariva <bastien.bellariva@ynov.com>
2018-05-10 10:36:20 +0200

    correction du script d'import (ligne creation db)

--- commandes_sh/scripts_creation_import.sh
+++ commandes_sh/scripts_creation_import.sh
@@ -4,8 +4,11 @@
 #apt-get update && apt-get upgrade -y
 #echo "Mise à jour packages système done"
 
+#Récupération du nom de la BDD a créer
+#DBName = "$1"
+
 #Lancement du script "creation-base" : crée la base d_vigne
-mysql -u root -p d_vigne < ../scripts_SQL/creation_base.sql
+mysql -u root -p <../scripts_SQL/creation-base.sql
 
 #Lancement du script "import_recolte" : importe toutes les valeurs avec le numéro de département faisant l'unicité des lignes
 #mysql -u root -p d_vigne < ../scripts_SQL/import_recolte.sql
