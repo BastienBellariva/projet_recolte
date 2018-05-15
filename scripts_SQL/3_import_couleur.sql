0d84e6078828de124687cff8475e6fc8d4338ba2
BastienBellariva <bastien.bellariva@ynov.com>
2018-05-15 10:01:55 +0200

    Rajout de l'import des table finale à partir des tables imports

--- scripts_SQL/3_import_couleur.sql
+++ scripts_SQL/3_import_couleur.sql
@@ -1,4 +1,4 @@
--- Table couleur : stocke toutes les couleurs de vin
+-- On crée la table stockant toutes les couleurs contenues dans le CSV, sans vérification
 CREATE TABLE t_couleur_import (
   i_libelle_couleur VARCHAR(30) NOT NULL,
   i_information_couleur VARCHAR(80) NOT NULL
@@ -10,3 +10,19 @@ FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 
+-- Création de la table t_couleur finale et propre, avec ID
+CREATE TABLE t_couleur (
+  id_couleur INT(4) NOT NULL AUTO_INCREMENT,
+  libelle_couleur VARCHAR(30) NOT NULL,
+  information_couleur VARCHAR(80) NOT NULL,
+  CONSTRAINT pk_id_couleur PRIMARY KEY(id_couleur)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Remplissage de cette table à partir de la table import
+INSERT INTO t_couleur (
+  libelle_couleur
+  information_couleur )
+  SELECT DISTINCT
+    i_libelle_couleur
+    i_information_couleur
+  FROM t_couleur_import
--- scripts_SQL/4_import_departement.sql
+++ scripts_SQL/4_import_departement.sql
@@ -1,4 +1,4 @@
--- Table département : stocke tous les départements de France
+-- On crée la table stockant tous les départements contenus dans le CSV, sans vérification.
 CREATE TABLE t_departement_import (
   i_numero_departement VARCHAR(4) NOT NULL,
   i_libelle_departement VARCHAR(30) NOT NULL
@@ -9,3 +9,20 @@ TABLE t_departement_import
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
+
+-- Creation de la table t_departement finale et propre, avec ID
+CREATE TABLE t_departement (
+  id_departement INT(4) NOT NULL AUTO_INCREMENT,
+  numero_departement VARCHAR(4) NOT NULL,
+  libelle_departement VARCHAR(30) NOT NULL,
+  CONSTRAINT pk_id_departement PRIMARY KEY(id_departement)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Remplissage de cette table à partir de la table import
+INSERT INTO t_departement (
+  numero_departement,
+  libelle_departement )
+  SELECT DISTINCT
+    i_numero_departement,
+    i_libelle_departement
+  FROM t_departement_import;
